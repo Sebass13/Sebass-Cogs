db482ef5a8338d09c85b6c6895405a66d6a9a860
Sebass13 <sebikele@gmail.com>
2017-07-06 13:01:21 -0500

    Update neato.py

--- neato/neato.py
+++ neato/neato.py
@@ -1,1 +1,18 @@
+import discord
+import re
 
+class Neato:
+    """Neato"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+        if re.search(r'\bneat\b', message.content) != None:
+            await self.bot.say("*neato")
+
+
+def setup(bot):
+    bot.add_cog(Neato(bot))
