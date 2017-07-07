f4781edbb0aa17408a97726024148194959a964b
Sebass13 <sebikele@gmail.com>
2017-07-06 20:52:57 -0500

    Update shutup.py

--- shutup/shutup.py
+++ shutup/shutup.py
@@ -49,9 +49,7 @@ class ShutUp:
             return
         if message.author == self.bot.user:
             return
-          
-        
-        await self.bot.send_message(message.channel, self.shutup[server.id]["tts"], tts=True) 
+        await self.bot.send_message(message.channel, "*neato")
 
 def check_folder():
     if not os.path.exists("data/shutup"):
