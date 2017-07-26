666c11e62141ff919046491b8be88ea919425ff5
Sebass13 <sebikele@gmail.com>
2017-07-25 23:25:36 -0500

    Update penis.py

--- penis/penis.py
+++ penis/penis.py
@@ -19,7 +19,7 @@ class Penis:
         random.setstate(state)
         if user.id == "199974404560519178":
             await self.bot.say("Size: 8=========================================D")
-        elif user.id == "313519333265506307":
+        elif user.id in ["313519333265506307", "323247765431779338"]:
             await self.bot.say("`Error: Penis not found for ID#313519333265506307`")
         else:
             await self.bot.say("Size: " + dong)
