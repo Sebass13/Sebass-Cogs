134e2774243724b569bb62a9314dfae7c91c0f5f
Sebass13 <sebikele@gmail.com>
2017-08-17 22:30:05 -0500

    add in fan.py

---
+++ fan/fan.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+try:
+    from pyHS100 import SmartPlug
+except Exception as e:
+    raise RuntimeError("You must run `pip3 install pyHS100`.") from e
+
+def _toggle_fan():
+    if plug.state == "OFF":
+        plug.turn_on()
+    else:
+        plug.turn_off()
+    
+class Fan:
+    """Turn on my fan!"""
+    
+    def __init__(self, bot):
+        self.bot = bot
+        self.fan = SmartPlug("192.168.0.175")
+        
+    @commands.command()
+    async def fan(self, mode: str = "toggle"):
+        if mode.upper() == fan.state:
+            await.bot.say("The fan is already " + mode.lower() + "!")
+        else:
+            _toggle_fan()
