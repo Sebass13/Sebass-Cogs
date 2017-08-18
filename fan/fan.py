4f8b5ad18f3058a42e843251b1b60e340e8455f4
Sebass13 <sebikele@gmail.com>
2017-08-17 22:42:39 -0500

    update fan.py

--- fan/fan.py
+++ fan/fan.py
@@ -5,12 +5,6 @@ try:
     from pyHS100 import SmartPlug
 except Exception as e:
     raise RuntimeError("You must run `pip3 install pyHS100`.") from e
-
-def _toggle_fan():
-    if plug.state == "OFF":
-        plug.turn_on()
-    else:
-        plug.turn_off()
     
 class Fan:
     """Turn on my fan!"""
@@ -20,12 +14,16 @@ class Fan:
         self.fan = SmartPlug("192.168.0.175")
         
     @commands.command()
-    async def fan(self, *, mode: str = "toggle"):
+    async def fan(self, mode: str = "toggle"):
         await self.bot.say("Your piece of shit function ran!")
         if mode.upper() == fan.state:
             await self.bot.say("The fan is already " + mode.lower() + "!")
         else:
-            _toggle_fan()
+            if fan.state == "OFF":
+                fan.turn_on()
+            else:
+                fan.turn_off()
+            await self.bot.say("The fan is now " + mode.lower() + "!")
             
 def setup(bot):
     bot.add_cog(Fan(bot))
