5cdd47b79f528666ff485f005a24214e03bab099
Sebass13 <sebikele@gmail.com>
2017-08-18 23:44:54 -0500

    update fan.py

--- fan/fan.py
+++ fan/fan.py
@@ -16,7 +16,7 @@ class Fan:
         self.fan_on = True
         
     @commands.command()
-    async def fan(self, ctx, *, mode: str = "toggle"):
+    async def fan(self, ctx, mode: str = "toggle"):
         await self.bot.say(ctx)
         if not self.fan_on and not (ctx.message.author.id == self.bot.settings.owner):
             await self.bot.say("The fan's state can not currently be changed.")
