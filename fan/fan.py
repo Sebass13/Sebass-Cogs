f1441df90e6c66feee5bdf596d73e85768911edd
Sebass13 <sebikele@gmail.com>
2017-08-18 23:33:00 -0500

    update fan.py

--- fan/fan.py
+++ fan/fan.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from .utils import checks
 
 try:
     from pyHS100 import SmartPlug
@@ -12,9 +13,14 @@ class Fan:
     def __init__(self, bot):
         self.bot = bot
         self.plug = SmartPlug("192.168.0.175")
+        self.fan_on = True
         
     @commands.command()
-    async def fan(self, *, mode: str = "toggle"):
+    async def fan(self, ctx, *, mode: str = "toggle"):
+        if not self.fan_on and not checks.is_owner_check(ctx):
+            await self.bot.say("The fan's state can not currently be changed.")
+            return
+        
         if mode.upper() == self.plug.state:
             await self.bot.say("The fan is already " + mode.lower() + "!")
         else:
@@ -24,6 +30,17 @@ class Fan:
                 self.plug.turn_off()
             await self.bot.say("The fan is now " + self.plug.state.lower() + "!")
             
+    @commands.command()
+    @checks.is_owner)
+    async def yesfan(self)
+        self.fan_on = True
+        await self.bot.say("Fan powers enabled!")
+        
+    @commands.command()
+    @checks.is_owner)
+    async def nofan(self)
+        self.fan_on = False  
+        await self.bot.say("Fan powers disabled!")
             
 def setup(bot):
     bot.add_cog(Fan(bot))
