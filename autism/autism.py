544c7b805066bd756a1524d0bda7267698279f9b
Sebass13 <sebikele@gmail.com>
2017-09-24 22:10:49 -0500

    Fixed bug where post could be modified to include autistic b's after edits

--- autism/autism.py
+++ autism/autism.py
@@ -1,48 +1,12 @@
 import discord
 
-def makeEmbed(*, name=None, icon=None, colour=0x171848, values={}):
-    '''Creates an embed messasge with specified inputs'''
-
-    # Create an embed object with the specified colour
-    embedObj = discord.Embed(colour=colour)
-
-    # Set the author and URL
-    embedObj.set_author(name=name, icon_url=icon)
-
-    # Create all of the fields
-    for i in values:
-        if values[i] == '':
-            values[i] = 'None'
-        embedObj.add_field(name=i, value='{}'.format(values[i]))
-
-    # Return to user
-    return embedObj
-
 class Autism:
     """Autism"""
     
     def __init__(self, bot):
         self.bot = bot
         
-    def makeEmbed(*, name=None, icon=None, colour=0x171848, values={}):
-        '''Creates an embed messasge with specified inputs'''
-
-        # Create an embed object with the specified colour
-        embedObj = discord.Embed(colour=colour)
-
-        # Set the author and URL
-        embedObj.set_author(name=name, icon_url=icon)
-
-        # Create all of the fields
-        for i in values:
-            if values[i] == '':
-                values[i] = 'None'
-            embedObj.add_field(name=i, value='{}'.format(values[i]))
-
-        # Return to user
-        return embedObj
-
-    async def on_message(self, message):
+    async def _autism_checker(self, message):
         if message.author == self.bot.user:
             return
         content = message.clean_content
@@ -56,6 +20,12 @@ class Autism:
                 await self.bot.send_message(message.channel, embed=new_embed)
             except discord.errors.Forbidden:
                 await self.bot.send_message(message.channel, "delet this ^^ becuz I can't ://///////////")
+        
+    async def on_message(self, message):
+        await self._autism_checker(message)
+                
+    async def on_message_edit(self, old_message, new_message):
+        await self._autism_checker(new_message)
 
 
 def setup(bot):
