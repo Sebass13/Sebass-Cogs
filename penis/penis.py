37a350f3d1d10ba7ebcc65aad2635ff363d63683
Sebass13 <sebikele@gmail.com>
2018-02-03 15:53:37 -0600

    Update penis.py

--- penis/penis.py
+++ penis/penis.py
@@ -1,6 +1,7 @@
 import discord
 import random
 from discord.ext import commands
+from typing import overload
 
 class Penis:
     """Penis related commands."""
@@ -23,6 +24,22 @@ class Penis:
             await self.bot.say("`Error: Penis not found for ID#" + user.id + "`")
         else:
             await self.bot.say("Size: " + dong)
+    
+    async def penis(self, *, user):
+        """Detects user's penis length
+
+        This is 100% accurate."""
+        state = random.getstate()
+        random.seed(user.id)
+        dong = "8{}D".format("=" * random.randint(0, 30))
+        random.setstate(state)
+        if user.id == "199974404560519178":
+            await self.bot.say("Size: 8=========================================D")
+        elif user.id in ["313519333265506307", "323247765431779338"]:
+            await self.bot.say("`Error: Penis not found for ID#" + user.id + "`")
+        else:
+            await self.bot.say("Size: " + dong)
+        
 
 
 def setup(bot):
