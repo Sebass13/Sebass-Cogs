e94ccafe5de009e6c59c386b7a9cd7d3787dcea2
Sebass13 <sebikele@gmail.com>
2018-02-12 17:42:04 -0600

    Create catfact.py

---
+++ catfact/catfact.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import requests
+
+class CatFact:
+    """Penis related commands."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def catfact(self, *, user : discord.Member):
+        """Random Cat Facts!"""
+        fact = requests.get(r'https://catfact.ninja/fact').json()['fact']
+        await self.bot.say(fact)
+
+def setup(bot):
+    bot.add_cog(CatFact(bot))
