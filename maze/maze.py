8ba82ef4df0d54c6e5dbeb2f79749272dc5f2611
Sebass13 <sebikele@gmail.com>
2018-02-17 01:16:36 -0600

    Update maze.py

--- maze/maze.py
+++ maze/maze.py
@@ -309,7 +309,7 @@ class MazeCog:
         
         for em in choices:
             await self.bot.add_reaction(msgobj, em)
-            await self.bot.say(em)
+            await self.bot.say(list(choices))
         
         choice = True
         while maze.player != maze.target:
